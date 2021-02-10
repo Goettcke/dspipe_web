2c1e4a7ce032c59da84c116e275344e6c9976ece
Goettcke <goettcke@imada.sdu.dk>
2021-02-10 21:02:42 +0100

    finished the basic admin panel

--- project/admin.py
+++ project/admin.py
@@ -15,20 +15,53 @@ def create_user():
     password = request.form.get('password')
     user_type = request.form.get('user_type')
 
+    print(email)
+    print(name)
+    print(password)
+    print(user_type)
 
+    user = User.query.filter_by(email=email).first()
+    print(user)
+    if user:  # if the user exists we redirect back to signup page, so user can login
+        print(f"User {user} already exists")
+        flash('Account already exists - try logging in')
+        render_template("example_profile_page.html", name='Account already exists - try logging in') # Maybe this works
+        print(f"User {user} already exists")
+    else:
+        print("User doesn't exist - let's create him")
+        new_user = User(email=email, name=name, user_type=user_type, password=generate_password_hash(password, method='sha256'))
+        db.session.add(new_user)
+        db.session.commit()
+    return redirect(url_for('main.admin_panel'))
+
+
+@admin.route('/update_user_type', methods=['POST'])
+@login_required
+def update_user_type():
+    # Code to validate and add user to database
+    email = request.form.get('user_email')
+    user_type = request.form.get('user_type')
+    print(email)
+    print(user_type)
     user = User.query.filter_by(email=email).first()
 
     if user:  # if the user exists we redirect back to signup page, so user can login
-        flash('Account already exists - try logging in')
-        return redirect(url_for('main.admin_panel'))
-    try:
-
-        new_user = User(email=email, name=name, user_type=user_type, password=generate_password_hash(password, method='sha256'))
-
-        db.session.add(new_user)
+        user.user_type = user_type
         db.session.commit()
-    except:
-        flash('Please ensure all fields are filled out!')
         return redirect(url_for('main.admin_panel'))
+    else:
+       return render_template("example_profile_page.html", name='Account already exists - try logging in') # Maybe this works
 
-    return redirect(url_for('main.admin_panel'))
+@admin.route('/delete_user', methods=['POST'])
+@login_required
+def delete_user():
+    # Code to validate and add user to database
+    email = request.form.get('user_email')
+    user = User.query.filter_by(email=email).first()
+
+    if user: # if the user exists we redirect back to signup page, so user can login
+        db.session.delete(user)
+        db.session.commit()
+        return redirect(url_for('main.admin_panel'))
+    else:
+        return render_template("example_profile_page.html", name="Couldn't delete user - try logging in") # Maybe this works
--- project/models.py
+++ project/models.py
@@ -20,7 +20,7 @@ class Todo(db.Model):
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    user_type = db.Column(db.String(30), unique=True)
+    user_type = db.Column(db.String(30))
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
